5e07375f8cd6de11d0467d71f674cb078813d6ec
alpheratz0 <alpheratz99@protonmail.com>
2023-08-03 09:51:52 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -2,12 +2,10 @@
 # This program is free software.
 
 VERSION=0.0.1
-
 CC=cc
 INCS=-I/usr/X11R6/include
 CFLAGS=-pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
 LDLIBS=-lxcb
 LDFLAGS=-L/usr/X11R6/lib -s
-
 PREFIX=/usr/local
 MANPREFIX=$(PREFIX)/share/man
